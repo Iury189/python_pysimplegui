3b046478da45d51827db5606b1435fe22a34688a
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-02-04 14:47:52 -0300

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,26 +1,24 @@
 from PySimpleGUI import PySimpleGUI as psgui
-import time
 
 # Layout
 psgui.theme('Reddit')
 layout = [ 
-    [psgui.Text('Digite seu nome: '),psgui.Input(key='nome', size=(25,1))],
-    [psgui.Text('Digite seu stand: '),psgui.Input(key='stand', size=(25,1))],
+    [psgui.Text('Digite seu nome: '),psgui.Input(key='nome', size=(30,1))],
+    [psgui.Text('Digite seu stand: '),psgui.Input(key='stand', size=(30,1))],
     [psgui.Button('Concluir'), psgui.Button('Sair')]
 ]
 
 # Janela
-janela = psgui.Window('Python Bizarre Adventure', layout)
+janela = psgui.Window('Python Bizarre Adventure', layout, enable_close_attempted_event=True)
 while True:
     eventos, valores = janela.read()
     # Caso seja encerrado ou clicado no botão Sair
-    if eventos in (psgui.WINDOW_CLOSED, 'Sair'):
+    if eventos in (psgui.WINDOW_CLOSE_ATTEMPTED_EVENT, 'Sair') and psgui.popup_yes_no('Deseja finalizar a aplicação?') == 'Yes':
         break
     # Caso seja clicado em Concluir
     if eventos == 'Concluir':
 	# Caso os campos não forem vazios
         if valores['nome'] != '' and valores['stand'] != '':
-            print ('Golden Wing!')
-            time.sleep(3)
+            psgui.Popup('Nome: ' + valores['nome'], 'Stand: ' + valores['stand'])
             break
 janela.close()
